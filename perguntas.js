6cd5629f05abcccac4d6d79957f1bde1a1648bf3
R4ph4e <117936088+R4ph4e@users.noreply.github.com>
2024-08-30 16:06:03 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Você prefere qual jogador?",
+        alternativas: [
+            {
+                texto: "A) Cristiano Ronaldo",
+                afirmacao: [
+                    "Você é um fã de futebol tradicional, com preferência por times e jogadores consagrados e reconhecidos pelo mundo. Gosta de torcer para os gigantes e admira o estilo de jogo mais clássico."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Messi",
+                afirmacao: [
+                    "Seu gosto é mais voltado para o futebol moderno e dinâmico. Prefere jogadores inovadores e times que estão se destacando recentemente, acompanhando as tendências e novas estrelas do esporte."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual time você prefere?",
+        alternativas: [
+            {
+                texto: "A) Athletico Paranaense",
+                afirmacao: [
+                    "Você é um fã de futebol tradicional, com preferência por times e jogadores consagrados e reconhecidos pelo mundo. Gosta de torcer para os gigantes e admira o estilo de jogo mais clássico.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Coritiba",
+                afirmacao: [
+                    "Seu gosto é mais voltado para o futebol moderno e dinâmico. Prefere jogadores inovadores e times que estão se destacando recentemente, acompanhando as tendências e novas estrelas do esporte."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu time favorito?",
+        alternativas: [
+            {
+                texto: "A) Real Madrid",
+                afirmacao: [
+                    "Você é um fã de futebol tradicional, com preferência por times e jogadores consagrados e reconhecidos pelo mundo. Gosta de torcer para os gigantes e admira o estilo de jogo mais clássico."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Barcelona",
+                afirmacao: [
+                    "Seu gosto é mais voltado para o futebol moderno e dinâmico. Prefere jogadores inovadores e times que estão se destacando recentemente, acompanhando as tendências e novas estrelas do esporte."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual time você escolhe?",
+        alternativas: [
+            {
+                texto: "A) Manchester United",
+                afirmacao: [
+                    "Você é um fã de futebol tradicional, com preferência por times e jogadores consagrados e reconhecidos pelo mundo. Gosta de torcer para os gigantes e admira o estilo de jogo mais clássico."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) Manchester City",
+                afirmacao: [
+                    "Seu gosto é mais voltado para o futebol moderno e dinâmico. Prefere jogadores inovadores e times que estão se destacando recentemente, acompanhando as tendências e novas estrelas do esporte."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual jogador você prefere?",
+        alternativas: [
+            {
+                texto: "A) Haaland",
+                afirmacao: [
+                    "Você é um fã de futebol tradicional, com preferência por times e jogadores consagrados e reconhecidos pelo mundo. Gosta de torcer para os gigantes e admira o estilo de jogo mais clássico."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Mbappé",
+                afirmacao: [
+                    "Seu gosto é mais voltado para o futebol moderno e dinâmico. Prefere jogadores inovadores e times que estão se destacando recentemente, acompanhando as tendências e novas estrelas do esporte."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses times você torce mais?",
+        alternativas: [
+            {
+                texto: "A) Corinthians",
+                afirmacao: [
+                    "Você é um fã de futebol tradicional, com preferência por times e jogadores consagrados e reconhecidos pelo mundo. Gosta de torcer para os gigantes e admira o estilo de jogo mais clássico."
+                ],
+            },
+            {
+                texto: "B) Palmeiras",
+                afirmacao: [
+                    "Seu gosto é mais voltado para o futebol moderno e dinâmico. Prefere jogadores inovadores e times que estão se destacando recentemente, acompanhando as tendências e novas estrelas do esporte."
+                ],
+            },
+        ]
+    },
+];
